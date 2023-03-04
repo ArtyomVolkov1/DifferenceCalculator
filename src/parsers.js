6ca3d529d0b49854e8e8a3cdadf88ef1d1afc167
Artyom Volkov <artemvolk2099@yandex.ru>
2023-03-04 17:50:46 +0300

    Update

--- src/parsers.js
+++ src/parsers.js
@@ -5,7 +5,6 @@ const parse = (data, format) => {
     case 'json':
       return JSON.parse(data);
     case 'yml':
-    case 'yaml':
       return yaml.load(data);
     default:
       throw new Error(`Unknown format: ${format}`);
