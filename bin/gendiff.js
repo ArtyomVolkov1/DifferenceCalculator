b60d821093a7f2c938d4bdfc2efdd9b892e7412b
Artyom Volkov <artemvolk2099@yandex.ru>
2023-03-04 17:49:48 +0300

    Update

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,7 +6,6 @@ import genDiff from '../src/index.js';
 const program = new Command();
 
 program
-  .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.8.0')
   .argument('<filepath1>', 'path to file1')
