b9e5639b008dbecc34c33bed88a33894a03c2df5
Artyom Volkov <artemvolk2099@yandex.ru>
2023-03-04 13:45:15 +0300

    Update

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,11 +6,12 @@ import genDiff from '../src/index.js';
 const program = new Command();
 
 program
+  .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.8.0')
   .argument('<filepath1>', 'path to file1')
   .argument('<filepath2>', 'parth to file2')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
     console.log(genDiff(filepath1, filepath2, program.opts().format));
   });
